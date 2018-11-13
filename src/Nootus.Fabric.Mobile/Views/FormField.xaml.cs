727e7db6b288bf6344ad139bf9f238f485851af3
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2018-11-13 07:23:40 +0530

    Completed FormField control

--- src/Nootus.Fabric.Mobile/Views/FormField.xaml.cs
+++ src/Nootus.Fabric.Mobile/Views/FormField.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Xamarin.Forms;
+﻿using Nootus.Fabric.Mobile.Behaviors;
+using Nootus.Fabric.Mobile.Converters;
+using Nootus.Fabric.Mobile.Validations;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Nootus.Fabric.Mobile.Views
@@ -11,43 +16,107 @@ namespace Nootus.Fabric.Mobile.Views
 			InitializeComponent ();
 		}
 
-        public static readonly BindableProperty EntryHeaderTextProperty = BindableProperty.Create(
-                                                         propertyName: "EntryHeaderText",
-                                                         returnType: typeof(string),
-                                                         declaringType: typeof(FormField),
-                                                         defaultBindingMode: BindingMode.OneTime,
-                                                         propertyChanged: EntryHeaderTextPropertyChanged);
-
-        public string EntryHeaderText
+        private string headerText;
+        public string HeaderText
         {
-            get { return (string) base.GetValue(EntryHeaderTextProperty); }
-            set { base.SetValue(EntryHeaderTextProperty, value); }
+            get { return headerText;  }
+            set { Header.Text = headerText = value; }
         }
 
-        private static void EntryHeaderTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        private Style headerStyle;
+        public Style HeaderStyle
+        {
+            get { return headerStyle; }
+            set { Header.Style = headerStyle = value; }
+        }
+
+        private Style errorStyle;
+        public Style ErrorStyle
+        {
+            get { return errorStyle; }
+            set { Error.Style = errorStyle = value; }
+        }
+
+        private Color errorLineColor;
+        public Color ErrorLineColor
+        {
+            get { return errorLineColor; }
+            set { errorLineColor = value; }
+        }
+
+        public static readonly BindableProperty FieldProperty = BindableProperty.Create(
+                                                         propertyName: "Field",
+                                                         returnType: typeof(ValidatableText<string>),
+                                                         declaringType: typeof(FormField),
+                                                         defaultBindingMode: BindingMode.TwoWay,
+                                                         propertyChanged: FieldPropertyChanged);
+
+        public ValidatableText<string> Field
+        {
+            get { return (ValidatableText<string>)base.GetValue(FieldProperty); }
+            set { base.SetValue(FieldProperty, value); }
+        }
+
+        private static void FieldPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var control = (FormField)bindable;
-            control.EntryHeader.Text = (string) newValue;
+            ValidatableText<string> field = (ValidatableText<string>) newValue;
+            control.Entry.SetBinding(Entry.TextProperty, new Binding("Value", source: field));            
+            control.Error.SetBinding(Label.TextProperty, new Binding("Errors", source: field, converter: new FirstValidationErrorConverter()));
+            DataTrigger trigger = new DataTrigger(typeof(Entry))
+            {
+                Binding = new Binding("IsValid", source: field),
+                Value = false
+            };
+
+
+            Setter setter = new Setter
+            {
+                Property = LineColorBehavior.LineColorProperty,
+                Value = control.ErrorLineColor
+            };
+
+            trigger.Setters.Add(setter);
+            control.Entry.Triggers.Add(trigger);
         }
 
-
-        public static readonly BindableProperty EntryHeaderStyleProperty = BindableProperty.Create(
-                                                         propertyName: "EntryHeaderText",
-                                                         returnType: typeof(Style),
+        public static readonly BindableProperty TextChangedCommandProperty = BindableProperty.Create(
+                                                         propertyName: "TextChangedCommand",
+                                                         returnType: typeof(ICommand),
                                                          declaringType: typeof(FormField),
                                                          defaultBindingMode: BindingMode.OneTime,
-                                                         propertyChanged: EntryHeaderStylePropertyChanged);
+                                                         propertyChanged: TextChangedCommandPropertyChanged);
 
-        public Style EntryHeaderStyle
-        {
-            get { return (Style) base.GetValue(EntryHeaderStyleProperty); }
-            set { base.SetValue(EntryHeaderStyleProperty, value); }
-        }
-
-        private static void EntryHeaderStylePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        private static void TextChangedCommandPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var control = (FormField)bindable;
-            control.EntryHeader.Style = (Style) newValue;
+            control.CommandBehavior.Command = (ICommand) newValue;
         }
     }
 }
+
+/*
+ * 
+ * 
+        public static readonly BindableProperty ValidatableEntryProperty = BindableProperty.Create(
+                                                         propertyName: "ValidatableEntry",
+                                                         returnType: typeof(ValidatableText<string>),
+                                                         declaringType: typeof(FormField),
+                                                         defaultBindingMode: BindingMode.TwoWay,
+                                                         propertyChanged: ValidatableEntryPropertyChanged);
+
+        public ValidatableText<string> ValidatableEntry
+        {
+            get { return (ValidatableText<string>)base.GetValue(ValidatableEntryProperty); }
+            set { base.SetValue(ValidatableEntryProperty, value); }
+        }
+
+        private static void ValidatableEntryPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (FormField)bindable;
+            ValidatableText<string> entry = (ValidatableText<string>)newValue;
+            control.Entry.Text = entry.Value;
+        }
+ * 
+ * 
+ */
