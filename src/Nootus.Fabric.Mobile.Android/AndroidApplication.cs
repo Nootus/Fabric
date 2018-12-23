85cfe5a911096b15359bb03a2044ff01fe2813f5
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2018-12-23 20:49:19 +0530

    Added DialogService and project references

---
+++ src/Nootus.Fabric.Mobile.Android/AndroidApplication.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using System;
+
+namespace Nootus.Fabric.Mobile.Droid
+{
+    public class AndroidApplication : Application, Application.IActivityLifecycleCallbacks
+    {
+        public static Activity MainActivity { get; private set; }
+
+        public AndroidApplication(IntPtr handle, JniHandleOwnership transer)
+          : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            RegisterActivityLifecycleCallbacks(this);
+        }
+
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+            UnregisterActivityLifecycleCallbacks(this);
+        }
+
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            MainActivity = activity;
+            var scaleFactor = activity.Resources.DisplayMetrics.Density;
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+        }
+
+        public void OnActivityPaused(Activity activity)
+        {
+        }
+
+        public void OnActivityResumed(Activity activity)
+        {
+            MainActivity = activity;
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+            MainActivity = activity;
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+        }
+    }
+}
---
+++ src/Nootus.Fabric.Mobile.Android/Services/DialogService.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Nootus.Fabric.Mobile.Droid.Services;
+using Nootus.Fabric.Mobile.Dialog;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: Dependency(typeof(DialogService))]
+namespace Nootus.Fabric.Mobile.Droid.Services
+{
+    public class DialogService : IDialogService
+    {
+        private Android.Views.View nativeView;
+        private DialogView dialogView;
+        private Android.App.Dialog dialog;
+
+        public void InitializeDialog()
+        {
+            this.dialogView = new DialogView(this);
+            var renderer = dialogView.GetOrCreateRenderer();
+
+            nativeView = renderer.View;
+
+            dialog = new Android.App.Dialog(AndroidApplication.MainActivity);
+            dialog.RequestWindowFeature((int)WindowFeatures.NoTitle);
+            dialog.SetCancelable(false);
+            dialog.SetContentView(nativeView);
+            Window window = dialog.Window;
+            window.SetLayout(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
+            // window.ClearFlags(WindowManagerFlags.DimBehind);
+            window.SetBackgroundDrawable(new ColorDrawable(Android.Graphics.Color.Transparent));
+        }
+
+        public void DisplayLoading()
+        {
+            if (dialogView.IsInitialized)
+            {
+                dialogView.InitializeLoading();
+                dialog.Show();
+            }
+        }
+
+        public void DisplayAlert(AlertMode mode, string message)
+        {
+            dialogView.InitializeAlert(mode, message);
+            dialog.Window.AddFlags(WindowManagerFlags.DimBehind);
+            dialog.Show();
+        }
+
+        public async void DisplayToast(AlertMode mode, string message)
+        {
+            dialogView.InitializeToast(mode, message);
+            dialog.Window.ClearFlags(WindowManagerFlags.DimBehind);
+            dialog.Show();
+            await Task.Delay(2000);
+            Hide();
+        }
+
+        public void Hide()
+        {
+            if (dialogView.IsInitialized)
+            {
+                dialogView.Hide();
+                dialog.Hide();
+            }
+        }
+    }
+
+    internal static class PlatformExtension
+    {
+        public static IVisualElementRenderer GetOrCreateRenderer(this VisualElement bindable)
+        {
+            var renderer = Platform.GetRenderer(bindable);
+            if (renderer == null)
+            {
+                renderer = Platform.CreateRendererWithContext(bindable, AndroidApplication.MainActivity);
+                Platform.SetRenderer(bindable, renderer);
+            }
+            return renderer;
+        }
+    }
+}
